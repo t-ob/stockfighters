e8890ae16262e5b7a6b59a42a50e42a213c7372c
Tom O'Brien <thomas.obrien86@gmail.com>
2015-12-13 00:04:08 +0000

    Initial commit

---
+++ clj/stockfighters/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stockfighters
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clj/stockfighters/test/stockfighters/core_test.clj
@@ -0,0 +1,7 @@
+(ns stockfighters.core-test
+  (:require [clojure.test :refer :all]
+            [stockfighters.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
