9d3ca437f898f091d9dccc7f0919628c8e7bc1aa
Tom O'Brien <tomobrien@Neils-MBP.office.lumi.do>
2015-12-16 16:57:20 +0000

    Add status and restart

--- clj/stockfighters/src/stockfighters/gm.clj
+++ clj/stockfighters/src/stockfighters/gm.clj
@@ -6,7 +6,17 @@
 (def api-url
   "https://api.stockfighter.io/gm")
 
+(defn status [api-key instance-id]
+  (let [endpoint (str (url api-url "instances" instance-id))]
+    @(http/get endpoint
+               {:headers {"X-Starfighter-Authorization" api-key}})))
+
 (defn start [api-key level-name]
   (let [endpoint (str (url api-url "levels" level-name))]
+    @(http/post endpoint
+                {:headers {"X-Starfighter-Authorization" api-key}})))
+
+(defn restart [api-key instance-id]
+  (let [endpoint (str (url api-url "instances" instance-id "restart"))]
     @(http/post endpoint
                 {:headers {"X-Starfighter-Authorization" api-key}})))
