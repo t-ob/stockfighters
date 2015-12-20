2883b851d330c9b4225ff50b431af045341463b8
Tom O'Brien <thomas.obrien86@gmail.com>
2015-12-20 17:22:09 +0000

    Add async libs

--- clj/stockfighters/project.clj
+++ clj/stockfighters/project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [aleph "0.4.1-beta2"]
                  [http-kit "2.1.18"]
                  [cheshire "5.5.0"]
                  [com.cemerick/url "0.1.1"]])
